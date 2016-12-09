c99dc2329ee147922ecd3aae3066e9ecf2429c34
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
-var merge = require('merge-deep');
-var Bliss = require('bliss');
+var merge = require('merge-deep')
+var Bliss = require('bliss')
 
-exports.name = 'bliss';
-exports.inputFormats = ['bliss', 'html'];
-exports.outputFormat = 'html';
+exports.name = 'bliss'
+exports.inputFormats = ['bliss', 'html']
+exports.outputFormat = 'html'
 
-exports.compile = function compile (str, opts) {
-  var bliss = new Bliss(opts);
+exports.compile = function (str, opts) {
+  var bliss = new Bliss(opts)
   return function (locals) {
-    locals = locals.context || locals || {};
-    locals = merge({}, opts.context || {}, locals);
-    return bliss.compile(str, {context: locals})();
-  };
-};
+    locals = locals.context || locals || {}
+    locals = merge({}, opts.context || {}, locals)
+    return bliss.compile(str, {context: locals})()
+  }
+}
